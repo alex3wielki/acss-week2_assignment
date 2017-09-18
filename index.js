784c4c92744c5287a2557c004a7d79e168b7ccad
Alex Ironside <aleksander3wielki@gmail.com>
2017-09-18 16:40:24 -0400

    done all but the sounds

--- index.js
+++ index.js
@@ -1,34 +1,82 @@
 let heroes = [
-    { 'name': 'Prof. Xavier', 'twitter': '@profx', 'pic': 'http://www.animatedimages.org/data/media/450/animated-marvel-avatar-image-0004.gif' },
-    { 'name': 'Spiderman', 'twitter': '@spidey', 'pic': 'http://www.animatedimages.org/data/media/450/animated-marvel-avatar-image-0008.gif' },
-    { 'name': 'Wolverine', 'pic': 'http://www.animatedimages.org/data/media/450/animated-marvel-avatar-image-0011.gif', 'twitter': '@logan' }
+  { 'name': 'Prof. Xavier', 'twitter': '@profx', 'pic': 'http://www.animatedimages.org/data/media/450/animated-marvel-avatar-image-0004.gif' },
+  { 'name': 'Spiderman', 'twitter': '@spidey', 'pic': 'http://www.animatedimages.org/data/media/450/animated-marvel-avatar-image-0008.gif' },
+  { 'name': 'Wolverine', 'pic': 'http://www.animatedimages.org/data/media/450/animated-marvel-avatar-image-0011.gif', 'twitter': '@logan' }
 ];
-
 
 let moreHeroes = [
-    { 'name': 'Cyclops', 'twitter': '@oneye', 'pic': 'http://www.animatedimages.org/data/media/450/animated-marvel-avatar-image-0005.gif' },
-    { 'name': 'Storm', 'twitter': '@rainsitpours', 'pic': 'http://www.animatedimages.org/data/media/450/animated-marvel-avatar-image-0007.gif' },
-    { 'name': 'Phoenix', 'twitter': '@jeangrey', 'pic': 'http://www.animatedimages.org/data/media/450/animated-marvel-avatar-image-0016.gif' }
+  { 'name': 'Cyclops', 'twitter': '@oneye', 'pic': 'http://www.animatedimages.org/data/media/450/animated-marvel-avatar-image-0005.gif' },
+  { 'name': 'Storm', 'twitter': '@rainsitpours', 'pic': 'http://www.animatedimages.org/data/media/450/animated-marvel-avatar-image-0007.gif' },
+  { 'name': 'Phoenix', 'twitter': '@jeangrey', 'pic': 'http://www.animatedimages.org/data/media/450/animated-marvel-avatar-image-0016.gif' }
 ];
-function render(obj){
-    let snippet = `
-    <article class="dt w-100 bb b--black-05 pb2 mt2">
-        <div class="dtc w2 w3-ns v-mid">
-            <img src="${obj.pic}" alt="Image of ${obj.name}" class="ba b--black-10 db br-100 w2 w3-ns h2 h3-ns"/>
+/**
+ * On click,
+ * an object which is taken from mapped array of two arrays above,
+ * It returns the ready HTML,
+ * is put into the render snippet fction.
+ * which is then appended to main.
+ */
+function mapArray(arr) {
+  let snippet = Array.from(arr.map(obj => `
+    <article class='dt w-100 bb b--black-05 pb2 mt2'>
+        <div class='dtc w2 w3-ns v-mid'>
+            <img src='${obj.pic}' alt='Image of ${obj.name}' class='ba b--black-10 db br-100 w2 w3-ns h2 h3-ns'/>
         </div>
-        <div class="dtc v-mid pl3">
-            <h1 class="f6 f5-ns fw6 lh-title black mv0">${obj.name}</h1>
-            <h2 class="f6 fw4 mt0 mb0 black-60">@yg</h2>
+        <div class='dtc v-mid pl3'>
+            <h1 class='f6 f5-ns fw6 lh-title black mv0'>${obj.name}</h1>
+            <h2 class='f6 fw4 mt0 mb0 black-60'>${obj.twitter}</h2>
         </div>
-        <div class="dtc v-mid">
-            <form class="w-100 tr">
-                <button class="f6 button-reset bg-white ba b--black-10 dim pointer pv1 black-60" type="submit">+ Follow</button>
+        <div class='dtc v-mid'>
+            <form class='w-100 tr'>
+                <button class='following' class='f6 button-reset bg-white ba b--black-10 dim pointer pv1 black-60' type='submit'>+ Follow</button>
             </form>
         </div>
     </article>
-    `;
-    return snippet;
+`));
+  return snippet;
+};
+
+let snippet = mapArray(heroes);
+for (let i = 0; i < heroes.length; i++) {
+  document.querySelector('main').insertAdjacentHTML('beforeend', snippet[i]);
+  updateEventListeners(heroes[i]);
 }
-let hero = render(heroes[0]);
-console.log(ziemniak);
-//testing ssh
+
+// Jest to array a nie string. Dlatego problem xD Ale jednoczesnie dobrze bo ma byc array.
+snippet = mapArray(moreHeroes);
+function* idMaker() {
+  let index = 0;
+  while (index < moreHeroes.length)
+    yield index++;
+}
+
+let gen = idMaker();
+document.querySelector('a').addEventListener('click', function () {
+  let index = gen.next().value;
+  index < 6 ? document.querySelector('main').insertAdjacentHTML('beforeend', snippet[index]) : console.warn('Out of heroes');
+  /*                              Finding this ^^^^^^^^^^^^^ function took SOOOOOOO LONG*/
+  updateEventListeners(moreHeroes[index]);
+});
+
+function updateEventListeners(hero) {
+  let followButtons = document.querySelectorAll('button.following');
+
+  for (let i = 0; i < followButtons.length; i++) {
+    if (!followButtons[i].classList.contains('has-event-listener')) {
+      let followButton = followButtons[i];
+      
+      followButton.className += 'has-event-listener';
+      followButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        if (followButton.textContent == '+ Follow') {
+          followButton.textContent = 'Following';
+          console.info(`following ${hero.name}`);
+        }
+        else {
+          followButton.textContent = '+ Follow';
+          console.info(`unfollowing ${hero.name}`);
+        }
+      });
+    }
+  };
+};
